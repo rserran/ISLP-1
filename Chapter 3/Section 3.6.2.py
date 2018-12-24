6044bc6400052f2d9a4fd0c28344c92236bed0b1
Arpan Ganguli <arpanganguli@icloud.com>
2018-12-24 14:20:29 +0000

    Create Section 3.6.2
    Added first piece of code. Some of the items remaining are -
    
    1. Adding the prediction function - I need to convert the np.ndarray indices into an integer format before prediction.
    2. Adding residual plots - It requires some additional coding which I expect to do over the holidays. I will upload the code once I have done it. This article creates equivalent codes really well - https://medium.com/@emredjan/emulating-r-regression-plots-in-python-43741952c034. It's absolutely essential that one draws residual plots since they visually explain a lot more than just (adj.-)R^2 etc.
    3. Adding plots of prediction functions and hatvalues - Again, TBD

---
+++ Chapter 3/Section 3.6.2.py
@@ -0,0 +1,40 @@
+# 3.6.2 Simple Linear Regression
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 20 02:29:16 2018
+
+@author: arpanganguli
+"""
+
+# import statistical tools
+import numpy as np
+import pandas as pd
+import sklearn
+from statsmodels.formula.api import ols
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+from sklearn import datasets
+
+# import data visualisation tools
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import dataset.
+Boston = pd.read_csv("/Users/arpanganguli/Documents/Finance : ML/ISLR/Datasets/Boston.csv", index_col = "Unnamed: 0")
+"""
+use the absolute path in your own computer like I have used in mine above
+"""
+
+# basic exploration of data
+print(Boston.head())
+print(Boston.corr())
+
+# fit model through linear regression
+Y = Boston['medv']
+X = Boston['lstat']
+model = ols("Y~X", data = Boston).fit()
+print(model.summary())
+
+# plot graph with regression line
+print(sns.regplot(X, Y, data = Boston, color = 'g'))
