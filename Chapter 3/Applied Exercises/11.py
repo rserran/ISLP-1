e0f6df02b20559eaa02f84f9e8a571609cb967f6
Arpan Ganguli <arpanganguli@icloud.com>
2019-01-04 03:03:02 +0000

    Update 11.py

--- Chapter 3/Applied Exercises/11.py
+++ Chapter 3/Applied Exercises/11.py
@@ -30,54 +30,111 @@ print(reg_1.summary())
 # 11.b. run regression x~y
 reg_2 = ols("x~y+0", data = data).fit()
 print(reg_2.summary())
+
+# 11.f. run regression WITH intercept
+reg_3 = ols("y~x", data = data).fit()
+print(reg_3.summary())
+
+reg_4 = ols("x~y", data = data).fit()
+print(reg_4.summary())
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 RESULTS (NOT PART OF CODE)
 
 runfile('/Users/arpanganguli/Documents/Finance/ISLR/General_Code.py', wdir='/Users/arpanganguli/Documents/Finance/ISLR')
                             OLS Regression Results                            
 ==============================================================================
-Dep. Variable:                      y   R-squared:                       0.806
-Model:                            OLS   Adj. R-squared:                  0.804
-Method:                 Least Squares   F-statistic:                     412.2
-Date:                Fri, 04 Jan 2019   Prob (F-statistic):           4.55e-37
-Time:                        02:57:15   Log-Likelihood:                -138.29
-No. Observations:                 100   AIC:                             278.6
-Df Residuals:                      99   BIC:                             281.2
+Dep. Variable:                      y   R-squared:                       0.770
+Model:                            OLS   Adj. R-squared:                  0.767
+Method:                 Least Squares   F-statistic:                     330.9
+Date:                Fri, 04 Jan 2019   Prob (F-statistic):           2.46e-33
+Time:                        03:01:25   Log-Likelihood:                -144.94
+No. Observations:                 100   AIC:                             291.9
+Df Residuals:                      99   BIC:                             294.5
 Df Model:                           1                                         
 Covariance Type:            nonrobust                                         
 ==============================================================================
                  coef    std err          t      P>|t|      [0.025      0.975]
 ------------------------------------------------------------------------------
-x              1.9142      0.094     20.303      0.000       1.727       2.101
+x              2.0371      0.112     18.191      0.000       1.815       2.259
 ==============================================================================
-Omnibus:                        3.395   Durbin-Watson:                   2.044
-Prob(Omnibus):                  0.183   Jarque-Bera (JB):                3.218
-Skew:                           0.186   Prob(JB):                        0.200
-Kurtosis:                       3.796   Cond. No.                         1.00
+Omnibus:                        4.100   Durbin-Watson:                   2.108
+Prob(Omnibus):                  0.129   Jarque-Bera (JB):                3.424
+Skew:                          -0.382   Prob(JB):                        0.180
+Kurtosis:                       3.488   Cond. No.                         1.00
 ==============================================================================
 
 Warnings:
 [1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
                             OLS Regression Results                            
 ==============================================================================
-Dep. Variable:                      x   R-squared:                       0.806
-Model:                            OLS   Adj. R-squared:                  0.804
-Method:                 Least Squares   F-statistic:                     412.2
-Date:                Fri, 04 Jan 2019   Prob (F-statistic):           4.55e-37
-Time:                        02:57:15   Log-Likelihood:                -62.596
-No. Observations:                 100   AIC:                             127.2
-Df Residuals:                      99   BIC:                             129.8
+Dep. Variable:                      x   R-squared:                       0.770
+Model:                            OLS   Adj. R-squared:                  0.767
+Method:                 Least Squares   F-statistic:                     330.9
+Date:                Fri, 04 Jan 2019   Prob (F-statistic):           2.46e-33
+Time:                        03:01:25   Log-Likelihood:                -60.705
+No. Observations:                 100   AIC:                             123.4
+Df Residuals:                      99   BIC:                             126.0
 Df Model:                           1                                         
 Covariance Type:            nonrobust                                         
 ==============================================================================
                  coef    std err          t      P>|t|      [0.025      0.975]
 ------------------------------------------------------------------------------
-y              0.4212      0.021     20.303      0.000       0.380       0.462
+y              0.3779      0.021     18.191      0.000       0.337       0.419
 ==============================================================================
-Omnibus:                        0.204   Durbin-Watson:                   2.146
-Prob(Omnibus):                  0.903   Jarque-Bera (JB):                0.134
-Skew:                           0.088   Prob(JB):                        0.935
-Kurtosis:                       2.965   Cond. No.                         1.00
+Omnibus:                        4.479   Durbin-Watson:                   2.112
+Prob(Omnibus):                  0.107   Jarque-Bera (JB):                3.796
+Skew:                           0.428   Prob(JB):                        0.150
+Kurtosis:                       3.422   Cond. No.                         1.00
+==============================================================================
+
+Warnings:
+[1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
+                            OLS Regression Results                            
+==============================================================================
+Dep. Variable:                      y   R-squared:                       0.769
+Model:                            OLS   Adj. R-squared:                  0.767
+Method:                 Least Squares   F-statistic:                     326.0
+Date:                Fri, 04 Jan 2019   Prob (F-statistic):           6.13e-33
+Time:                        03:01:25   Log-Likelihood:                -144.25
+No. Observations:                 100   AIC:                             292.5
+Df Residuals:                      98   BIC:                             297.7
+Df Model:                           1                                         
+Covariance Type:            nonrobust                                         
+==============================================================================
+                 coef    std err          t      P>|t|      [0.025      0.975]
+------------------------------------------------------------------------------
+Intercept     -0.1214      0.104     -1.170      0.245      -0.327       0.085
+x              2.0258      0.112     18.057      0.000       1.803       2.248
+==============================================================================
+Omnibus:                        3.991   Durbin-Watson:                   2.137
+Prob(Omnibus):                  0.136   Jarque-Bera (JB):                3.315
+Skew:                          -0.375   Prob(JB):                        0.191
+Kurtosis:                       3.482   Cond. No.                         1.12
+==============================================================================
+
+Warnings:
+[1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
+                            OLS Regression Results                            
+==============================================================================
+Dep. Variable:                      x   R-squared:                       0.769
+Model:                            OLS   Adj. R-squared:                  0.767
+Method:                 Least Squares   F-statistic:                     326.0
+Date:                Fri, 04 Jan 2019   Prob (F-statistic):           6.13e-33
+Time:                        03:01:25   Log-Likelihood:                -60.513
+No. Observations:                 100   AIC:                             125.0
+Df Residuals:                      98   BIC:                             130.2
+Df Model:                           1                                         
+Covariance Type:            nonrobust                                         
+==============================================================================
+                 coef    std err          t      P>|t|      [0.025      0.975]
+------------------------------------------------------------------------------
+Intercept      0.0277      0.045      0.614      0.541      -0.062       0.117
+y              0.3796      0.021     18.057      0.000       0.338       0.421
+==============================================================================
+Omnibus:                        4.565   Durbin-Watson:                   2.120
+Prob(Omnibus):                  0.102   Jarque-Bera (JB):                3.883
+Skew:                           0.432   Prob(JB):                        0.144
+Kurtosis:                       3.429   Cond. No.                         2.18
 ==============================================================================
 
 Warnings:
